c44a882bedc111a0cc2376b6a6e7e0b3fea52163
ShubhiG30 <104990861+ShubhiG30@users.noreply.github.com>
2023-06-19 20:48:49 +0530

    main.py

--- python/main.py
+++ python/main.py
@@ -1,15 +1,17 @@
+
+# this code is to check that twilio has been connected to your phn
 from twilio.rest import Client
 
 # Your Account SID from twilio.com/console
-account_sid = "ACc310fadde63a758578b47082b59e759c"
+account_sid = "write your account sid of twilio"
 # Your Auth Token from twilio.com/console
-auth_token  = "3af5ddf1f322b6b1a0722c772d09a273"
+auth_token  = "your twilio token"
 
 client = Client(account_sid, auth_token)
 
 message = client.messages.create(
     from_="whatsapp:+14155238886",
     body='Your appointment is coming up on July 21 at 3PM',
-    to='whatsapp:+919653094417'
+    to='whatsapp:[your registered number]'
 )
 print(message.sid)
